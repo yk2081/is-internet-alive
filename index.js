1af4b9a25598a7f2b215da79168ad74f5dc0fc5d
unknown <ykim751@gatech.edu>
2021-08-19 14:08:32 -0400

    data file used

--- index.js
+++ index.js
@@ -16,33 +16,36 @@
 const NetworkSpeed = require('network-speed');  // ES5
 const testNetworkSpeed = new NetworkSpeed();
 
-getNetworkDownloadSpeed();
-
 async function getNetworkDownloadSpeed() {
-  const baseUrl = 'https://eu.httpbin.org/stream-bytes/5000000';
+  const baseUrl = 'http://cleoemr.com:9000/dummy_data';
   const fileSizeInBytes = 5000000;
-  const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
   let date = new Date().toString();
-  console.log(date + " : " + speed.mbps + " mbps");
+  try {
+    let speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+    console.log(date + " : " + speed.mbps + " mbps");
+  } catch (err) {
+    console.log(date + " : " + err.toString());
+  }
+
 }
 
 //getNetworkUploadSpeed();
 
-async function getNetworkUploadSpeed() {
-  const options = {
-    hostname: 'www.google.com',
-    port: 80,
-    path: '/catchers/544b09b4599c1d0200000289',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  const fileSizeInBytes = 2000000
-  const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-  let date = new Date().toLocaleDateString();
-  console.log(date + " : " + speed.mbps + " mbps");
-}
+// async function getNetworkUploadSpeed() {
+//   const options = {
+//     hostname: 'www.google.com',
+//     port: 80,
+//     path: '/catchers/544b09b4599c1d0200000289',
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   };
+//   const fileSizeInBytes = 2000000
+//   const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+//   let date = new Date().toLocaleDateString();
+//   console.log(date + " : " + speed.mbps + " mbps");
+// }
 
 setInterval(function() {
     getNetworkDownloadSpeed();
--- readme.txt
+++ readme.txt
@@ -1,1 +1,7 @@
-README
+install git, nodejs, python 2.7
+http://download.microsoft.com/download/b/e/d/bedddfc4-55f4-4748-90a8-ffe38a40e89f/vs2015.3.com_enu.iso
+Start C++ project
+
+npm install --global --production windows-build-tools --vs2015
+
+https://spin.atomicobject.com/2019/03/27/node-gyp-windows/
